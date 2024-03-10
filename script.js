bf2b7981b3ca082a6877ffd5e79614e8a19f28a5
CamiloTello002 <camilojtello5@gmail.com>
2024-03-10 14:40:07 -0500

    Post requests are sent to the server from the html

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+  // Get the form reference
+  const form = document.getElementById("myForm");
+
+  // Here we trigger the post request
+  form.submit();
+}
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
   console.log("route found!");
+  console.log(req.body.username);
   res.status(201).json({
     status: "success",
     message: "I already read your message hah",
